0c230970b7ab53e068682bdcb680d703f75bddc9
HanyMohamed <hanymohamedsaadeldean@gmail.com>
2025-06-14 22:43:22 +0300

    ♻️refactor: Footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,37 +1,22 @@
 <template>
   <footer
-    class="relative mt-[120px] bg-secondary border-white/60 border-t text-white md:mt-[240px] py-8 md:pt-14 px-11 sm:px-7"
-  >
+    class="relative mt-[120px] bg-secondary border-white/60 border-t text-white md:mt-[240px] py-8 md:pt-14 px-11 sm:px-7">
     <div class="container relative mx-auto text-white max-w-[1320px]">
       <div
-        class="grid grid-cols-2 gap-16 border-b border-white/10 pb-6 md:grid-cols-3 md:gap-[72px] md:pb-12 lg:grid-cols-5"
-      >
+        class="grid grid-cols-2 gap-16 border-b border-white/10 pb-6 md:grid-cols-3 md:gap-[72px] md:pb-12 lg:grid-cols-5">
         <div class="col-span-2">
-          <img
-            src="~/assets/img/logo.svg"
+          <img src="~/assets/img/logo.svg"
             class="mb-12 inline-block object-contain w-[200px] h-[50px] md:w-[235px] md:h-[63px]"
-            alt="Mastry Clinics logo"
-            width="235px"
-            height="63px"
-          />
+            alt="Mastry Clinics logo" width="235px" height="63px" />
           <div class="content max-w-[288px]">
             <p class="text-base font-normal !text-white">
-              <!-- {{ $t("footer.logo.JustDream") }} -->
               ابقَ على تواصل معنا عبر منصات التواصل الاجتماعي!
             </p>
             <div class="flex flex-wrap gap-3 md:gap-4 mt-4">
-              <div
-                class="flex h-9 w-9 items-center justify-center rounded-full border border-white/10"
-                v-for="social in SocialMedia"
-                :key="social.id"
-              >
-                <a
-                  :href="social.link"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  class="text-white text-xl"
-                  v-html="social.icon"
-                ></a>
+              <div class="flex h-9 w-9 items-center justify-center rounded-full border border-white/10"
+                v-for="social in SocialMedia" :key="social.id">
+                <a :href="social.link" target="_blank" rel="noopener noreferrer" class="text-white text-xl"
+                  v-html="social.icon"></a>
               </div>
             </div>
           </div>
@@ -45,7 +30,7 @@
             <nuxt-link :to="localePath('aboutUs')">
               <p>{{ $t("footer.page.aboutUs") }}</p>
             </nuxt-link>
-            <nuxt-link :to="localePath('solutions')">
+            <nuxt-link :to="localePath('dental')">
               <p>{{ $t("footer.page.dental") }}</p>
             </nuxt-link>
             <nuxt-link :to="localePath('dermatology')">
@@ -68,19 +53,12 @@
           </div>
         </div>
         <div>
-          <img
-            src="~/assets/img/QR Code.svg"
-            alt="QR Code"
-            width="220px"
-            height="220px"
-            class="inline-block object-contain"
-          />
+          <img src="~/assets/img/QR Code.svg" alt="QR Code" width="220px" height="220px"
+            class="inline-block object-contain" />
         </div>
         <!-- </div> -->
       </div>
-      <h1
-        class="mt-4 px-4 text-center text-sm font-normal text-paragraph md:mt-6"
-      >
+      <h1 class="mt-4 px-4 text-center text-sm font-normal text-paragraph md:mt-6">
         {{ $t("footer.copyright") }}
       </h1>
     </div>
